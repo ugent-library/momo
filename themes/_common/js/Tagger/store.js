7e84204cac19a5b99c25cf97500c96ce8dee4ffe
netsensei <matthias@colada.be>
2021-05-31 13:29:51 +0200

    Changed: removed dummy tags.

--- themes/_common/js/Tagger/store.js
+++ themes/_common/js/Tagger/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 const state = {
   form: {
-    tags: ['foo', 'barfoobarfoobar', 'foo', 'barfoobarfoobar', 'foo', 'barfoobarfoobar', 'foo', 'barfoobarfoobar', 'foo', 'barfoobarfoobar', 'foo', 'barfoobarfoobar', 'foo', 'barfoobarfoobar']
+    tags: []
   }
 }
 
